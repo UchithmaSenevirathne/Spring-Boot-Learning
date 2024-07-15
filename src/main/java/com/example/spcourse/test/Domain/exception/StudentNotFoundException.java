e638ffc2c8bac65e46687f3c801a8155e117ae3c
UchithmaSenevirathne <uchithma@gmail.com>
2024-07-15 19:59:01 +0530

    pass custom exception | exception handlling

--- src/main/java/com/example/spcourse/test/Domain/exception/StudentNotFoundException.java
+++ src/main/java/com/example/spcourse/test/Domain/exception/StudentNotFoundException.java
@@ -2,6 +2,6 @@ package com.example.spcourse.test.Domain.exception;
 
 public class StudentNotFoundException extends RuntimeException{
     public StudentNotFoundException(String message){
-
+        super(message);
     }
 }
