112180da4dad1a1d2e58327792424a94e2331701
UchithmaSenevirathne <uchithma@gmail.com>
2024-07-15 20:07:19 +0530

    add Slf4j

--- src/main/java/com/example/spcourse/test/Domain/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/spcourse/test/Domain/exception/GlobalExceptionHandler.java
@@ -1,7 +1,9 @@
 package com.example.spcourse.test.Domain.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 
 @ControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 }
