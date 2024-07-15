7729991b1c3e814c95e1e9aeedf0a551f0bf8e6d
UchithmaSenevirathne <uchithma@gmail.com>
2024-07-15 18:35:33 +0530

    use generated type for auto generate id

--- src/main/java/com/example/spcourse/test/Domain/entity/Student.java
+++ src/main/java/com/example/spcourse/test/Domain/entity/Student.java
@@ -1,6 +1,8 @@
 package com.example.spcourse.test.Domain.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -12,6 +14,7 @@ import lombok.NoArgsConstructor;
 @Data
 public class Student {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
     private String address;
