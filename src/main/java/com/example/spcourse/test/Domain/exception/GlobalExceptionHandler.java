b73ccf48ac3317d098f3cf9127ded4d48092282e
UchithmaSenevirathne <uchithma@gmail.com>
2024-07-15 20:09:42 +0530

    create exception handle method | exception handlling

--- src/main/java/com/example/spcourse/test/Domain/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/spcourse/test/Domain/exception/GlobalExceptionHandler.java
@@ -1,9 +1,15 @@
 package com.example.spcourse.test.Domain.exception;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
+    @ExceptionHandler(StudentNotFoundException.class)
+    public ResponseEntity<String> handleStudentNotFoundException(StudentNotFoundException studentNotFoundException){
+
+    }
 }
