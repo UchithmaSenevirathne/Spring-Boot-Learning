a935f0da4c4768553f6591a01aa8abb17c4da7e7
UchithmaSenevirathne <uchithma@gmail.com>
2024-07-18 19:23:59 +0530

    create many to many realationship between student and subject

--- src/main/java/com/example/spcourse/test/Domain/entity/Student.java
+++ src/main/java/com/example/spcourse/test/Domain/entity/Student.java
@@ -1,9 +1,6 @@
 package com.example.spcourse.test.Domain.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -20,6 +17,7 @@ public class Student {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @ManyToMany(mappedBy = "enrolledStudent")
     private Set<Subject> enrolledSubject = new HashSet<>();
 
     private String name;
